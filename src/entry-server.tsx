177a49436c667b3fe69649c38fee0e77afd1635d
Eli <eli.axel.ang@gmail.com>
2025-09-14 11:19:50 +0800

    asdf

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,10 +9,12 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="/reset.css" />
+          <link rel="stylesheet" href="/global.css" />
           {assets}
         </head>
         <body>
-          <div id="app">{children}</div>
+          {children}
           {scripts}
         </body>
       </html>
